d03fcc322c9d9c3733fdbcc97da540b7a8ae70a6
Jhonatan <jhonatandaar@gmail.com>
2024-06-11 14:43:12 -0500

    chore: upgraded nvim config

--- users/jhonatan/nvim.nix
+++ users/jhonatan/nvim.nix
@@ -1,91 +1,162 @@
-{ ... }:
-let
-  nvim-config = {
-    enable = true;
-    colorschemes.tokyonight.enable = true;
+{ pkgs, ... }: 
+let 
+	nvim-config = {
+		enable = true; 
+		colorschemes.tokyonight.enable = true;
 
-    globals.mapleader = " ";
+		globals.mapleader = " ";
 
-    opts = {
-      tabstop = 2;
-      shiftwidth = 2;
-      expandtab = false;
-    };
+		opts = {
+			tabstop = 2;
+			shiftwidth = 2;
+			expandtab = false;
+		};
 
-    keymaps = [
-      {
-        key = ";";
-        action = ":";
-      }
-      {
-        key = "<leader>ca";
-        action = "<cmd>lua vim.lsp.buf.code_action()<CR>";
-        options = {
-          silent = true;
-          remap = false;
-        };
-      }
-      {
-        key = "<leader>fb";
-        action = "<cmd>Telescope buffers<CR>";
-        options = {
-          silent = true;
-          remap = false;
-        };
-      }
-      {
-        key = "<leader>ff";
-        action = "<cmd>Telescope find_files<CR>";
-        options = {
-          silent = true;
-          remap = false;
-        };
-      }
-      {
-        key = "<leader>f";
-        action = "<cmd>Telescope<CR>";
-        options = {
-          silent = true;
-          remap = false;
-        };
-      }
-      {
-        key = "<leader>cd";
-        action = "<cmd>TroubleToggle<CR>";
-        options = {
-          silent = true;
-          remap = false;
-        };
-      }
-      {
-        key = "<leader>cs";
-        action = "<cmd>Telescope treesitter<CR>";
-        options = {
-          silent = true;
-          remap = false;
-        };
-      }
-      {
-        key = "<leader>t";
-        action = "<cmd>ToggleTerm<CR>";
-        options = {
-          silent = true;
-          remap = false;
-        };
-      }
-      {
-        key = "<leader>e";
-        action = "<cmd>CHADopen<CR>";
-        options = {
-          silent = true;
-          remap = false;
-        };
-      }
-    ];
+		autoCmd = [
+			{
+				event = "LspAttach";
+				callback = {
+					__raw = ''
+						function(args)
+							local bufnr = args.buf
+							local client = vim.lsp.get_client_by_id(args.data.client_id)
+							if vim.tbl_contains({ 'null-ls' }, client.name) then  -- blacklist lsp
+								return
+							end
+							require("lsp_signature").on_attach({
+								bind = true,
+								handler_opts = {
+									border = "rounded"
+								}
+							}, bufnr)
+						end
+					'';
+				};
+			}
+		];
 
-    plugins = {
-      fidget.enable = true;
-      cmp = {
+		keymaps = [
+			{
+				key = ";";
+				action = ":";
+			}
+			{
+				key = "<leader>z";
+				action = "<cmd>Twilight<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>ca";
+				action = "<cmd>lua vim.lsp.buf.code_action()<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>gt";
+				action = "<cmd>Telescope lsp_type_definitions<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>ct";
+				action = "<cmd>lua require('lsp_signature').toggle_float_win()<CR>";
+				options = {
+					silent = false;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>gd";
+				action = "<cmd>Telescope lsp_definitions<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>gr";
+				action = "<cmd>Telescope lsp_references<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>fb";
+				action = "<cmd>Telescope buffers<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>ff";
+				action = "<cmd>Telescope find_files<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>f";
+				action = "<cmd>Telescope<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>cd";
+				action = "<cmd>TroubleToggle<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>cs";
+				action = "<cmd>Telescope treesitter<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>t";
+				action = "<cmd>ToggleTerm<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+			{
+				key = "<leader>e";
+				action = "<cmd>CHADopen<CR>";
+				options = {
+					silent = true;
+					remap = false;
+				};
+			}
+		];
+
+		extraPlugins = with pkgs.vimPlugins; [
+			{
+				plugin = lsp_signature-nvim;
+			}
+		];
+
+		plugins = {
+			fidget.enable = true;
+			statuscol.enable = true;
+			twilight.enable = true;
+			cmp = {
 				settings = {
 					autoEnableSources = true;
 					mapping = {
@@ -100,34 +171,35 @@ let
 						{ name = "buffer"; }
 					];
 				};
-        enable = true;
-      };
-      trouble.enable = true;
-      chadtree.enable = true;
-      lualine.enable = true;
-      bufferline.enable = true;
-      gitsigns = {
-        enable = true;
-        settings.current_line_blame = true;
-      };
-      telescope = {
-        enable = true;
-        extensions = { 
+				enable = true;
+			};
+			trouble.enable = true;
+			chadtree.enable = true;
+			lualine.enable = true;
+			bufferline.enable = true;
+			gitsigns = {
+				enable = true;
+				settings.current_line_blame = true;
+			};
+			telescope = {
+				enable = true;
+				extensions = { 
 					fzf-native.enable = true;
 					ui-select.enable = true;
 				};
-				defaults.wrap_results = true;
-      };
-      treesitter.enable = true;
-      lsp = {
-        enable = true;
-        servers = {
-          tsserver.enable = true; # TS/JS
-          cssls.enable = true; # CSS
-          tailwindcss.enable = true; # TailwindCSS
-          html.enable = true; # HTML
-          astro.enable = true; # AstroJS
-          svelte.enable = false; # Svelte
+				settings.defaults.wrap_results = true;
+			};
+			treesitter.enable = true;
+			lsp = {
+				enable = true;
+				servers = {
+					tsserver.enable = true; # TS/JS
+					cssls.enable = true; # CSS
+					tailwindcss.enable = true; # TailwindCSS
+					html.enable = true; # HTML
+					svelte.enable = false; # Svelte
+					nixd.enable = true; # Nix
+					jsonls.enable = true; # Json
           vuels.enable = false; # Vue
           pyright.enable = true; # Python
           marksman.enable = true; # Markdown
@@ -136,18 +208,18 @@ let
           bashls.enable = true; # Bash
           clangd.enable = true; # C/C++
           csharp-ls.enable = true; # C#
-        };
-      };
-      nvim-autopairs.enable = true;
-      toggleterm = {
-        enable = true;
-        settings = {
-          hide_numbers = false;
-          autochdir = true;
-          close_on_exit = true;
-          direction = "horizontal";
-        };
-      };
-    };
-  };
+				};
+			};
+			nvim-autopairs.enable = true;
+			toggleterm = {
+				enable = true;
+				settings = {
+					hide_numbers = false;
+					autochdir = true;
+					close_on_exit = true;
+					direction = "horizontal";
+				};
+			};
+		};
+	};
 in { programs.nixvim = nvim-config; }
