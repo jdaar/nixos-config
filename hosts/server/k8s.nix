3e6a8583c87169fd990927a7826670ac5403c900
jhonatan <jhonatandaar@gmail.com>
2026-05-03 22:13:02 -0500

    chore: added headlamp to k8s

--- hosts/server/k8s.nix
+++ hosts/server/k8s.nix
@@ -1,56 +1,59 @@
 { pkgs, ... }:
+let
+  namespaceYaml = pkgs.writeText "namespace.yaml" ''
+    apiVersion: v1
+    kind: Namespace
+    metadata:
+      name: projectcontour
+  '';
+
+  contourYaml = pkgs.writeText "contour.yaml" ''
+    apiVersion: helm.cattle.io/v1
+    kind: HelmChart
+    metadata:
+      name: contour
+      namespace: kube-system
+    spec:
+      chart: contour
+      repo: https://charts.bitnami.com/bitnami
+      targetNamespace: projectcontour
+      valuesContent: |
+        contour:
+          ingressController:
+            enabled: true
+  '';
+
+  headlampYaml = pkgs.writeText "headlamp.yaml" ''
+    apiVersion: helm.cattle.io/v1
+    kind: HelmChart
+    metadata:
+      name: headlamp
+      namespace: kube-system
+    spec:
+      chart: headlamp
+      repo: https://kubernetes.github.io/headlamp
+      targetNamespace: projectcontour
+      valuesContent: |
+        service:
+          type: NodePort
+          nodePort: 30000
+  '';
+
+  manifestDir = "/var/lib/rancher/k3s/server/manifests";
+in
 {
   services.k3s = {
     enable = true;
     role = "server";
     extraFlags = "--disable=traefik";
-    manifests = {
-      namespace = {
-        enable = true;
-        content = {
-          apiVersion = "v1";
-          kind = "Namespace";
-          metadata = {
-            name = "projectcontour";
-          };
-        };
-      };
-      contour = {
-        enable = true;
-        content = {
-          apiVersion = "helm.cattle.io/v1";
-          kind = "HelmChart";
-          metadata = {
-            name = "contour";
-            namespace = "kube-system";
-          };
-          spec = {
-            chart = "contour";
-            repo = "https://charts.bitnami.com/bitnami";
-            targetNamespace = "projectcontour";
-            valuesContent = "contour:\n  ingressController:\n    enabled: true\n";
-          };
-        };
-      };
-      headlamp = {
-        enable = true;
-        content = {
-          apiVersion = "helm.cattle.io/v1";
-          kind = "HelmChart";
-          metadata = {
-            name = "headlamp";
-            namespace = "kube-system";
-          };
-          spec = {
-            chart = "headlamp";
-            repo = "https://kubernetes.github.io/headlamp";
-            targetNamespace = "projectcontour";
-            valuesContent = "service:\n  type: NodePort\n  nodePort: 30000\n";
-          };
-        };
-      };
-    };
   };
+
+  systemd.tmpfiles.rules = [
+    "d ${manifestDir} 0755 root root -"
+    "L+ ${manifestDir}/namespace.yaml - - - - ${namespaceYaml}"
+    "L+ ${manifestDir}/contour.yaml - - - - ${contourYaml}"
+    "L+ ${manifestDir}/headlamp.yaml - - - - ${headlampYaml}"
+  ];
 
   networking.firewall = {
     allowedTCPPorts = [
