73457e3cfe49e8460d57d6349d34eb4015fb9c76
jhonatan <jhonatandaar@gmail.com>
2026-05-03 21:24:19 -0500

    chore: fixed firewall issues with xrdp

--- hosts/server/user-space.nix
+++ hosts/server/user-space.nix
@@ -22,7 +22,16 @@
 
   services.xrdp = {
     enable = true;
-    defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+    defaultWindowManager = "${pkgs.writeScript "startgnome" ''
+      #!/bin/sh
+      export XDG_CURRENT_DESKTOP=GNOME
+      export XDG_SESSION_DESKTOP=GNOME
+      export XDG_SESSION_TYPE=x11
+      export GDK_BACKEND=x11
+      export LIBGL_ALWAYS_SOFTWARE=1
+      export GNOME_DESKTOP_SESSION_ID=1
+      exec ${pkgs.gnome-session}/bin/gnome-session
+    ''}";
     openFirewall = true;
   };
   environment.variables.XDG_CURRENT_DESKTOP = "GNOME";
