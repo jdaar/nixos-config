30e55d6ff69cb4cdccb4878244bc6cbb30494286
jhonatan <jhonatandaar@gmail.com>
2026-05-03 21:45:20 -0500

    chore: rollback to gnome

--- hosts/server/user-space.nix
+++ hosts/server/user-space.nix
@@ -22,21 +22,17 @@
 
   services.xrdp = {
     enable = true;
-    defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+    defaultWindowManager = "${pkgs.writeScript "gnome-xrdp" ''
+      #!/bin/sh
+      export XDG_SESSION_TYPE=x11
+      export XDG_CURRENT_DESKTOP=GNOME
+      export GDK_BACKEND=x11
+      export LIBGL_ALWAYS_SOFTWARE=1
+      export GNOME_DESKTOP_SESSION_ID=1
+      exec ${pkgs.gnome-session}/bin/gnome-session
+    ''}";
     openFirewall = true;
   };
-  environment.etc."xrdp/startwm.sh".source = pkgs.writeScript "startwm.sh" ''
-    #!/bin/sh
-    if [ -z "$XDG_RUNTIME_DIR" ]; then
-      export XDG_RUNTIME_DIR="/run/user/$(id -u)"
-    fi
-    export XDG_SESSION_TYPE=x11
-    export XDG_CURRENT_DESKTOP=GNOME
-    export GDK_BACKEND=x11
-    export LIBGL_ALWAYS_SOFTWARE=1
-    export GNOME_DESKTOP_SESSION_ID=1
-    exec ${pkgs.gnome-session}/bin/gnome-session
-  '';
   services.libinput.enable = true;
   programs.zsh.enable = true;
 }
