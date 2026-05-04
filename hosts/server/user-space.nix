dc524133630088be0024471a1a98f08e282be0cd
jhonatan <jhonatandaar@gmail.com>
2026-05-03 21:28:23 -0500

    chore: gnome to xfce in server

--- hosts/server/user-space.nix
+++ hosts/server/user-space.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
 
   services.printing.enable = true;
 
@@ -22,20 +21,9 @@
 
   services.xrdp = {
     enable = true;
-    defaultWindowManager = "${pkgs.writeScript "startgnome" ''
-      #!/bin/sh
-      export XDG_CURRENT_DESKTOP=GNOME
-      export XDG_SESSION_DESKTOP=GNOME
-      export XDG_SESSION_TYPE=x11
-      export GDK_BACKEND=x11
-      export LIBGL_ALWAYS_SOFTWARE=1
-      export GNOME_DESKTOP_SESSION_ID=1
-      exec ${pkgs.gnome-session}/bin/gnome-session
-    ''}";
+    defaultWindowManager = "${pkgs.xfce.xfce4-session}/bin/startxfce4";
     openFirewall = true;
   };
-  environment.variables.XDG_CURRENT_DESKTOP = "GNOME";
-  environment.variables.XDG_SESSION_DESKTOP = "GNOME";
   services.libinput.enable = true;
   programs.zsh.enable = true;
 }
